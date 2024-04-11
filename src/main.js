cd348acdae34276e725f6eddbc4450f5c39bfbcb
FernandSilva <109560014+FernandSilva@users.noreply.github.com>
2024-04-11 19:57:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
-import { Client } from 'node-appwrite';
+const { Client, Query } = require("node-appwrite");
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+module.exports = async function (req, res) {
+    const client = new Client()
+        .setEndpoint(process.env.APPWRITE_ENDPOINT)
+        .setProject(process.env.APPWRITE_PROJECT_ID)
+        .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+    const database = new Database(client);
+    const followerId = req.payload.followerId;
+    const followingId = req.payload.followingId;
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+    // Find the relationship document
+    let response = await database.listDocuments(process.env.COLLECTION_ID, [
+        Query.equal('followerId', followerId),
+        Query.equal('followingId', followingId)
+    ]);
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+    if (response.documents.length > 0) {
+        // Relationship exists, delete it
+        await database.deleteDocument(process.env.COLLECTION_ID, response.documents[0].$id);
+        res.send('Follow relationship removed successfully');
+    } else {
+        res.send('No such follow relationship exists');
+    }
 };
+
